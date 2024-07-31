49cbdc949af4cc8262ea92f3f980859aeab451b0
CarlosOStaff <carlos.ovando@staffbridge.com.mx>
2024-07-31 17:32:15 -0600

    cambios

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -174,7 +174,7 @@ class AuthController extends Controller
                         <h3 style="font-style: italic; font-weight: bold; color: black;">Hola, este es un correo generado para la recuperación de tu contraseña.</h3>
                         <p style="font-style: italic; color: #555;">Sigue los pasos a continuación para poder cambiar tu contraseña:</p>
                         <p style="color: #555;">Haz clic en el siguiente enlace:</p>
-                        <a href="' . url('/nuevo-password/' . $idEncrypt) . '" style="display: inline-block; padding: 1vw 1.5vw; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">Cambiar contraseña</a>
+                        <a href="' .route('newpassword',$idEncrypt).'" style="display: inline-block; padding: 1vw 1.5vw; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">Cambiar contraseña</a>
                     </div>';
 
                 $mail->send();
